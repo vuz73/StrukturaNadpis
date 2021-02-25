034ee19a3f56073110b6a79e8dc80eb1f81a750f
vuz73 <vuz73@yandex.ru>
2021-02-25 14:42:50 +0300

    Добавьте файлы проекта.

---
+++ Struktura_nadpis/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+    Создать структуру "надпись" с полями:
+    - цвет (перечисление);
+    - текст (string).
+    
+    Написать функцию, которая принимает такую структуру как параметр и выводит в консоль надпись соответствующим цветом.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struktura_nadpis
+{
+    enum Color 
+    { 
+        Красный, Желтый, Зеленый, Синий, Голубой
+    }
+    class Program
+    {
+        struct Title
+        {
+            Color Tsvet;
+            string Text;            
+        }
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
